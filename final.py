da235c1a37cbcc00e8d568780125f49d16a2b0f1
Yuchen Wang <wangyuchen2017@yeah.net>
2019-04-04 21:52:39 -0400

    a huge update

---
+++ final.py
@@ -0,0 +1,23 @@
+def calculate_final(perc: int, scores: list, bonus: int) -> float:
+    """
+    Return a lower bound score for the final exam in order 
+    to get an A (85)
+    perc: the weight of the final exam
+    scores: the scores you have right now, 
+    key is a weight, value is the score you got
+    
+    >>> my_scores = [(0.2, 83), (0.25, 100), (0.05, 100)]
+    >>> perc = 0.5
+    >>> calculate_final(perc, my_scores, 0)
+    76.8
+    """
+    sum = 0
+    for pair in scores:
+        sum += pair[0] * pair[1]
+    lost = 100 * (1 - perc) - sum - bonus
+    return round(100 - ((15 - lost) / perc), 1)
+
+if __name__ == "__main__":
+    my_scores = [(0.2, 83), (0.25, 100), (0.05, 100)]
+    perc = 0.5
+    print(calculate_final(perc, my_scores, 0))
