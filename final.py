45cdc375b1c2bc91c46af6269863cbef38cbfd28
Yuchen Wang <wangyuchen2017@yeah.net>
2019-04-22 13:01:48 -0400

    huge update

--- final.py
+++ final.py
@@ -15,9 +15,9 @@ def calculate_final(perc: int, scores: list, bonus: int) -> float:
     for pair in scores:
         sum += pair[0] * pair[1]
     lost = 100 * (1 - perc) - sum - bonus
-    return round(100 - ((15 - lost) / perc), 1)
+    return round(100 - ((15.5 - lost) / perc), 1)
 
 if __name__ == "__main__":
-    my_scores = [(0.2, 83), (0.25, 100), (0.05, 100)]
-    perc = 0.5
+    my_scores = [(0.05, 100), (0.1, 100), (0.1, 94), (0.05, 95), (0.1, 100), (0.2, 100)]
+    perc = 0.4
     print(calculate_final(perc, my_scores, 0))
